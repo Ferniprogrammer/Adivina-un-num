5b395a59a4a8d011a321df792f3221124136aa86
ferniprogrammer <fsanchor@myuax.com>
2022-11-16 22:19:09 +0100

    Funcionan los niveles de dificultad

--- Codigo.py
+++ Codigo.py
@@ -8,14 +8,14 @@ elif dificultad == 2:
     num = random.randint(1,999)
     máx = 999
 elif dificultad == 3:
-    num = random.randint(1,9999)
-    máx = 9999
+    num = random.randint(1,999999)
+    máx = 999999
 elif dificultad == 4:
-    num = random.randint(1,99999)
-    máx = 99999
+    num = random.randint(1,999999999999)
+    máx = 999999999999
 else:
      print("no es una dificultad válida")
-def Adivina_el_num(num, guess, máx):
+def Adivina_el_num(num, guess):
     if num < guess < máx:
         print("Demasiado grande")
     elif 1 < guess < num:
@@ -29,11 +29,14 @@ def Adivina_el_num(num, guess, máx):
     else:
         print("No es un número, inntentalo otra vez")
         return
-guess1 = int(input("introduce un número del 1 al ", máx))
+print("introduce un número del 1 al ", máx)
+guess1 = int(input())
 Adivina_el_num(num,guess1)
-guess2 = int(input("introduce otro número del 1 al ", máx))
+print("introduce otro número del 1 al ", máx)
+guess2 = int(input())
 Adivina_el_num(num,guess2)
-guess3 = int(input("introduce un último número del 1 al ", máx))
+print("introduce un último número del 1 al ", máx)
+guess3 = int(input())
 if num == guess3:
     print("Ganastes :)")
 else:
